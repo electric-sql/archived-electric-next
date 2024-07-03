a03763159745f6e143d3eaeead1fc11fe259fc41
James Arthur <thruflo@gmail.com>
2024-07-03 20:10:55 +0200

    client: a few more tests.

--- client.test.ts
+++ client.test.ts
@@ -88,7 +88,7 @@ describe(`Shape`, () => {
 
     // Add an item.
     const id = uuidv4()
-    const title = `Item ${id}`
+    const title = `Test3 ${id}`
 
     await client.query(`insert into items (id, title) values ($1, $2)`, [id, title])
 
@@ -104,9 +104,114 @@ describe(`Shape`, () => {
     const expectedValue = new Map()
     expectedValue.set(`public-items-${id}`, {
       "id": id,
-      "title": `Item ${id}`,
+      "title": title,
     })
 
     expect(map).toEqual(expectedValue)
+  })
+
+  it(`should continually sync a shape/table`, async () => {
+    const { aborter, client } = context
+
+    // Add an item.
+    const id = uuidv4()
+    const title = `Test4 ${id}`
+
+    await client.query(`insert into items (id, title) values ($1, $2)`, [id, title])
+
+    const stream = new ShapeStream({
+      baseUrl: `http://localhost:3000`,
+      shape: { table: `items` },
+      signal: aborter.signal
+    })
+
+    const shape = new Shape(stream)
+    const map = await shape.sync()
+
+    const expectedValue = new Map()
+    expectedValue.set(`public-items-${id}`, {
+      "id": id,
+      "title": title,
+    })
+    expect(map).toEqual(expectedValue)
+
+    const hasUpdated = new Promise((resolve) => {
+      shape.subscribe(resolve)
+    })
+
+    const id2 = uuidv4()
+    const title2 = `Test4 ${id2}`
+
+    await client.query(`insert into items (id, title) values ($1, $2)`, [id2, title2])
+    await hasUpdated
+
+    expectedValue.set(`public-items-${id2}`, {
+      "id": id2,
+      "title": title2,
+    })
+    expect(shape.value).toEqual(expectedValue)
+
+    shape.unsubscribeAll()
+  }, 1000)
+
+  it(`should notify subscribers when the value changes`, async () => {
+    const { aborter, client } = context
+
+    // Add an item.
+    const id = uuidv4()
+    const title = `Test5 1 ${id}`
+
+    await client.query(`insert into items (id, title) values ($1, $2)`, [id, title])
+
+    const stream = new ShapeStream({
+      baseUrl: `http://localhost:3000`,
+      shape: { table: `items` },
+      signal: aborter.signal
+    })
+
+    const shape = new Shape(stream)
+    await shape.sync()
+
+    const hasChanged = new Promise((resolve) => {
+      shape.subscribe((value) => {
+        resolve(value)
+      })
+    })
+
+    // Add an item.
+    const id2 = uuidv4()
+    const title2 = `Test5 2 ${id2}`
+
+    await client.query(`insert into items (id, title) values ($1, $2)`, [id2, title2])
+    const value = await hasChanged
+
+    const expectedValue = new Map()
+    expectedValue.set(`public-items-${id}`, {
+      "id": id,
+      "title": title,
+    })
+    expectedValue.set(`public-items-${id2}`, {
+      "id": id2,
+      "title": title2,
+    })
+    expect(value).toEqual(expectedValue)
+
+    shape.unsubscribeAll()
+  }, 1000)
+
+  it(`should support unsubscribe`, async () => {
+    const { aborter, client } = context
+
+    const stream = new ShapeStream({
+      baseUrl: `http://localhost:3000`,
+      shape: { table: `items` },
+      signal: aborter.signal
+    })
+
+    const shape = new Shape(stream)
+    const subscriptionId = shape.subscribe((value) => { console.log(value) })
+    shape.unsubscribe(subscriptionId)
+
+    expect(shape.numSubscribers).toBe(0)
   })
 })
