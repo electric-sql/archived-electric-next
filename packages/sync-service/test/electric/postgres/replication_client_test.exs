2872278a29d73571c5845e99b1895e7000ae8c4c
Oleksii Sholik <oleksii@sholik.dev>
2024-07-24 15:47:24 +0300

    Create a test publication in a setup callback

--- packages/sync-service/test/electric/postgres/replication_client_test.exs
+++ packages/sync-service/test/electric/postgres/replication_client_test.exs
@@ -8,14 +8,41 @@ defmodule Electric.Postgres.ReplicationClientTest do
   @moduletag :capture_log
   @publication_name "test_electric_publication"
 
-  def create_publication_for_all_tables(conn),
-    do: Postgrex.query!(conn, "CREATE PUBLICATION #{@publication_name} FOR ALL TABLES", [])
+  describe "ReplicationClient init" do
+    setup {Support.DbSetup, :with_unique_db}
+    setup {Support.DbStructureSetup, :with_basic_tables}
+
+    setup do
+      %{
+        init_opts: [
+          publication_name: @publication_name,
+          transaction_received: {__MODULE__, :test_transaction_received, [self()]}
+        ]
+      }
+    end
+
+    test "creates an empty publication on startup if requested", %{
+      db_config: config,
+      init_opts: init_opts,
+      db_conn: conn
+    } do
+      init_opts = Keyword.put(init_opts, :try_creating_publication?, true)
+      assert {:ok, _} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
+
+      assert %{rows: [[@publication_name]]} =
+               Postgrex.query!(conn, "SELECT pubname FROM pg_publication", [])
+
+      assert %{rows: []} = Postgrex.query!(conn, "SELECT pubname FROM pg_publication_tables", [])
+    end
+  end
 
   describe "ReplicationClient against real db" do
     setup {Support.DbSetup, :with_unique_db}
     setup {Support.DbStructureSetup, :with_basic_tables}
 
-    setup do
+    setup %{db_conn: conn} do
+      create_publication_for_all_tables(conn)
+
       %{
         init_opts: [
           publication_name: @publication_name,
@@ -27,7 +54,6 @@ defmodule Electric.Postgres.ReplicationClientTest do
 
     test "calls a provided function when receiving it from the PG",
          %{db_config: config, init_opts: init_opts, db_conn: conn} do
-      create_publication_for_all_tables(conn)
       assert {:ok, _pid} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
 
       {:ok, _} =
@@ -42,8 +68,6 @@ defmodule Electric.Postgres.ReplicationClientTest do
 
     test "logs a message when connected & replication has started",
          %{db_config: config, init_opts: init_opts, db_conn: conn} do
-      create_publication_for_all_tables(conn)
-
       log =
         ExUnit.CaptureLog.capture_log(fn ->
           assert {:ok, _pid} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
@@ -61,28 +85,11 @@ defmodule Electric.Postgres.ReplicationClientTest do
       log =~ "Started replication from postgres"
     end
 
-    test "creates an empty publication on startup if requested", %{
-      db_config: config,
-      init_opts: init_opts,
-      db_conn: conn
-    } do
-      init_opts = Keyword.put(init_opts, :try_creating_publication?, true)
-      assert {:ok, _} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
-
-      assert %{rows: [[@publication_name]]} =
-               Postgrex.query!(conn, "SELECT pubname FROM pg_publication", [])
-
-      assert %{rows: []} = Postgrex.query!(conn, "SELECT pubname FROM pg_publication_tables", [])
-    end
-
     test "doesn't fail to start when publicaiton already exists", %{
       db_config: config,
-      init_opts: init_opts,
-      db_conn: conn
+      init_opts: init_opts
     } do
       init_opts = Keyword.put(init_opts, :try_creating_publication?, true)
-      create_publication_for_all_tables(conn)
-
       assert {:ok, _} = ReplicationClient.start_link(config ++ [init_opts: init_opts])
     end
   end
@@ -98,4 +105,7 @@ defmodule Electric.Postgres.ReplicationClientTest do
 
   def test_transaction_received(transaction, test_pid),
     do: send(test_pid, {:from_replication, transaction})
+
+  defp create_publication_for_all_tables(conn),
+    do: Postgrex.query!(conn, "CREATE PUBLICATION #{@publication_name} FOR ALL TABLES", [])
 end
